9ac42de4837fd45dbd65120f2cf6a5a998835a12
pboisver <pboisver@8d3ef213-1316-0410-8b69-af0d8d322f5f>
2013-06-06 12:06:00 +0000

    Initial commit

---
+++ bob/trunk/lundera/src/main/java/edu/umich/umms/HomeController.java
@@ -0,0 +1,41 @@
+package edu.umich.umms;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = DateFormat.getInstance().format(new Date());
+
+		model.addAttribute("serverTime", new Date().toString() + "ffffffffffffffff" );
+//		model.addAttribute("foo", StringUtils.reverse("bar"));
+
+		return "home";
+	}
+
+}
